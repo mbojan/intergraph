ee171930f8c771b312ec0b649d3fa138263d50e8
mbojan <mbojan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-17 00:40:51 +0000

    fixed DESCRIPTION and example data script
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/intergraph/pkg/intergraph@213 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- exdata.R
+++ exdata.R
@@ -1,43 +1,43 @@
 library(intergraph)
-library(igraph0)
+library(igraph)
 
 # directed igraph
-g <- igraph0::graph( c(2,1, 3,1, 4,1, 5,1, # star
+g <- igraph::graph( c(2,1, 3,1, 4,1, 5,1, # star
 	 6,7, 8,9, # two dyads
 	 10,11, 11,12, 12,13, 13,14, 14,12), # stem-leaf
-	 n=14, directed=TRUE)
+	 n=15, directed=TRUE)
 # add some vertex attributes
 vl <- letters[seq(1, vcount(g))]
-g <- igraph0::set.vertex.attribute(g, "label", value=vl)
+g <- igraph::set.vertex.attribute(g, "label", value=vl)
 # add some edge attributes
-m <- igraph0::get.edgelist(g)
+m <- igraph::get.edgelist(g)
 l <- matrix(vl[m+1], ncol=2)
 el <- apply(l, 1, paste, collapse="")
-g <- igraph0::set.edge.attribute(g, "label", value=el)
-g <- igraph0::set.graph.attribute(g, "layout", igraph0::layout.fruchterman.reingold)
+g <- igraph::set.edge.attribute(g, "label", value=el)
+g <- igraph::set.graph.attribute(g, "layout", igraph::layout.fruchterman.reingold)
 rm(vl, l, m, el)
 exIgraph <- g
 
 # undirected igraph
-exIgraph2 <- igraph0::as.undirected(exIgraph)
-exIgraph2 <- igraph0::set.edge.attribute(exIgraph2, "label", 
-	value=igraph0::get.edge.attribute(exIgraph, "label"))
+exIgraph2 <- igraph::as.undirected(exIgraph)
+exIgraph2 <- igraph::set.edge.attribute(exIgraph2, "label", 
+	value=igraph::get.edge.attribute(exIgraph, "label"))
 
 
 # copy as a 'network' object through adjacency matrix
-m <- igraph0::get.adjacency(exIgraph)
+m <- igraph::get.adjacency(exIgraph)
 g <- network::network(m, vertex.attr=list(label=V(exIgraph)$label),
     vertex.attrnames="label", directed=TRUE)
 network::set.vertex.attribute(g, "vertex.names", value=V(exIgraph)$label)
-network::set.edge.attribute(g, "label", igraph0::get.edge.attribute(exIgraph, "label"))
+network::set.edge.attribute(g, "label", igraph::get.edge.attribute(exIgraph, "label"))
 exNetwork <- network::network.copy(g)
 
 # copy as a 'network' object through adjacency matrix
-m <- igraph0::get.adjacency(exIgraph2)
+m <- igraph::get.adjacency(exIgraph2)
 g <- network::network(m, vertex.attr=list(label=V(exIgraph2)$label),
     vertex.attrnames="label", directed=FALSE)
 network::set.vertex.attribute(g, "vertex.names", value=V(exIgraph2)$label)
-network::set.edge.attribute(g, "label", igraph0::get.edge.attribute(exIgraph2, "label"))
+network::set.edge.attribute(g, "label", igraph::get.edge.attribute(exIgraph2, "label"))
 exNetwork2 <- network::network.copy(g)
 
 # save objects to files
