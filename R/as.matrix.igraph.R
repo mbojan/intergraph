08fe1a9ad204407beeefa70e11639ca37f2a98e9
mbojan <mbojan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-12 00:42:40 +0000

    as.matrix returns normal mats for igraph objs
    as.matrix method for igraph objects when asked to return adjacency
    matrices by default returns normal matrix instead of sparse (which is
    now igraph default). This can be changed with a new 'sparse' argument
    passed to igraph::get.adjacency.
    
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/intergraph/pkg/intergraph@251 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/as.matrix.igraph.R
+++ R/as.matrix.igraph.R
@@ -1,9 +1,9 @@
 
 # wrapper over get.adjacency / get.edgelist
-as.matrix.igraph <- function(x, matrix.type=c("adjacency", "edgelist"), attrname=NULL, ...)
+as.matrix.igraph <- function(x, matrix.type=c("adjacency", "edgelist"), attrname=NULL, sparse=FALSE, ...)
 {
   mt <- match.arg(matrix.type)
   switch(mt,
-      adjacency = igraph::get.adjacency(graph=x, attr=attrname, ...),
+      adjacency = igraph::get.adjacency(graph=x, attr=attrname, sparse=sparse, ...),
       edgelist = igraph::get.edgelist(graph=x, ...) )
 }
