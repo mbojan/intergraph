ba3d76a073c3f7d85a2f02f83464174485db551b
mbojan <mbojan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-12 00:42:51 +0000

    Completed utests for validating functions
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/intergraph/pkg/intergraph@257 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/utests/runit.validnet.R
+++ inst/utests/runit.validnet.R
@@ -15,3 +15,50 @@ test.validateEL <- function()
   options(op)
 }
 
+
+
+
+
+# Validating vertex DBs
+runit.validateVDB <- function()
+{
+  # Corrected data frame
+  df1 <- data.frame(id=1:5, x=c(1,2,3,2,1), y=c(3,2,1,2,3))
+  checkEquals(df1, validateVDB(df1))
+
+  # empty dataframe
+  df2 <- data.frame(id=numeric(0), x=character(0))
+  checkException( validateVDB(df2) )
+
+  # duplicated ids
+  df3 <- df1
+  df3$id[3] <- 1
+  checkException( validateVDB(df3))
+
+  # NAs
+  df4 <- df1
+  df4$id[2] <- NA
+  op <- options(warn=2)
+  checkException( validateVDB(df4))
+  options(op)
+}
+
+
+
+
+
+# Validating EDB vs VDB
+runit.validNetDB <- function()
+{
+  # Correct
+  edb <- data.frame( ego = 1:5, alter = c(2,3,2,5,4))
+  vdb <- data.frame(id=1:5, x=c(1,2,3,2,1), y=c(3,2,1,2,3))
+  checkTrue( validNetDB(edb, vdb))
+
+  
+  # some ids EL are not found in VDB
+  elist <- data.frame( ego = 1:5, alter = c(2,3,2,5,4))
+  vdb <- data.frame(id=1:4, x=c(1,2,3,4), y=c(3,2,1,2))
+  checkException( validNetDB(elist, vdb))
+}
+
