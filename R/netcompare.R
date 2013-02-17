9858234014bee4253400b6fb5e284cdc3ccc0e46
mbojan <mbojan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-17 01:37:31 +0000

    Added fixed netcompare
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/intergraph/pkg/intergraph@282 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/netcompare.R
+++ R/netcompare.R
@@ -1,117 +1,46 @@
-#============================================================================ 
-# comparing and testing for (near) equality of networks
-#
-# Result of comparison:
-#
-# 1. computed network-level comparisons
-#
-# 2. built-in network-level comparisons
-#
-# 3. attributes
-#
-# 3a. attribute presence
-#
-# 3b. identical common attributes
-#
-# NOTE: Makes use of non-exported generic functions
-
-
-compareEdges <- function(target, current, use.names=FALSE)
-{
-  tr <- try(getS3method("as.matrix", class=class(target)), silent=TRUE)
-  if(inherits(tr, "try-error"))
-    stop("cannot find 'as.matrix' method for class ", dQuote(class(target)))
-  tr <- try(getS3method("as.matrix", class=class(current)), silent=TRUE)
-  if(inherits(tr, "try-error"))
-    stop("cannot find 'as.matrix' method for class ", dQuote(class(current)))
-  mtar <- as.matrix(target, "adjacency")
-  mcur <- as.matrix(current, "adjacency")
-  # compare matrices (no dimnames)
-  if(use.names)
-    all.equal(mtar, mcur)
-  else all.equal( structure(mtar, dimnames=NULL), structure(mcur,
-            dimnames=NULL) )
-}
-
-
-# compare common components of a list (by name)
-# return a list of all.equal results
-compareAlist <- function(target, current)
-{
-  # common components
-  nams <- intersect(names(target), names(current))
-  if( length(nams) == 0 )
-    return(as.character(NA))
-  rval <- lapply(nams, function(n) all.equal( target[[n]], current[[n]]) )
-  names(rval) <- nams
-  rval
-}
-
-
-
-# given a lists of attributes compare
-
-compareAttributes <- function(target, current)
-{
-  # compare number of attributes
-  rval <- list()
-  rval$n <- c(target=length(target), current=length(current))
-  # compare names
-  pre <- list()
-  u <- union(names(target), names(current))
-  r <- t(sapply(u, function(a)
-      c( a %in% names(target),
-          a %in% names(current) )
-      ))
-  pre <- c(pre, list(r))
-  pre <- do.call("rbind", pre)
-  dimnames(pre) <- list(rownames(pre), c("target", "current"))
-  rval$presence <- pre
-  rval$bycomp <- compareAlist(target, current)
-  structure(rval, class="netcomparea")
-}
-
-
-
-
-
-#'Comparing and testing network objects
+#' Comparing and testing network objects
+#' 
+#' Compare or test network objects for (near) equality.
+#' 
+#' Arguments \code{target} and \code{current} can be network objects of one of
+#' the supported classes. They do not have to be of the same class though.
+#' 
+#' The function does a series of comparisons between \code{target} and
+#' \code{current}: \enumerate{ \item The network structure comparison is made
+#' based on adjacency matrices (mind this when using for huge networks).
+#' 
+#' \item Network/edge/vertex attributes are checked for presence in both
+#' objects.
+#' 
+#' \item Common network/edge/vertex attribures are checked for equality. } All
+#' the results are collected in a list of class \code{netcompare} with an
+#' associated \code{print} method.
+#' 
+#' If \code{test} is TRUE then instead of the detailed test results the function
+#' returns TRUE or FALSE depending on some of the checks resulted positively.
+#' Currently attribute checks are ignored, i.e. what is taken into account is:
+#' \enumerate{ \item Equivalence of adjacency matrices \item Directed /
+#' undirected character of the network \item Edge set size \item Vertex set size
+#' }
+#' 
+#' @param target,current network objects, currently \code{network} and
+#' \code{igraph} classes are supported
 #'
-#'Compare or test network objects for (near) equality.
+#' @param test logical, whether to perform the test or return comparison data,
+#' see Details
 #'
-#'Arguments \code{target} and \code{current} can be network objects of one of
-#'the supported classes. They do not have to be of the same class though.
+#' @param \dots other arguments, currently ignored
 #'
-#'The function does a series of comparisons between \code{target} and
-#'\code{current}: \enumerate{ \item The network structure comparison is made
-#'based on adjacency matrices (mind this when using for huge networks).
+#' @return Depending on the value of \code{test} either an object of class
+#' \code{netcompare} containing the results of all the tests (if
+#' \code{test=FALSE}) or (if \code{test=TRUE}) a logical whether or not the
+#' networks are (nearly) the same.
 #'
-#'\item Network/edge/vertex attributes are checked for presence in both
-#'objects.
+#' @seealso \code{\link{all.equal}}, \code{\link{identical}}
 #'
-#'\item Common network/edge/vertex attribures are checked for equality. } All
-#'the results are collected in a list of class \code{netcompare} with an
-#'associated \code{print} method.
+#' @export
 #'
-#'If \code{test} is TRUE then instead of the detailed test results the function
-#'returns TRUE or FALSE depending on some of the checks resulted positively.
-#'Currently attribute checks are ignored, i.e. what is taken into account is:
-#'\enumerate{ \item Equivalence of adjacency matrices \item Directed /
-#'undirected character of the network \item Edge set size \item Vertex set size
-#'}
-#'
-#'@aliases netcompare print.netcompare
-#'@param target,current network objects, currently \code{network} and
-#'\code{igraph} classes are supported
-#'@param test logical, whether to perform the test or return comparison data,
-#'see Details
-#'@param \dots other arguments, currently ignored
-#'@return Depending on the value of \code{test} either an object of class
-#'\code{netcompare} containing the results of all the tests (if
-#'\code{test=FALSE}) or (if \code{test=TRUE}) a logical whether or not the
-#'networks are (nearly) the same.
-#'@seealso \code{\link{all.equal}}, \code{\link{identical}}
-#'@examples
+#' @examples
 #'
 #'netcompare( as.igraph(exNetwork), exNetwork)
 #'netcompare( as.igraph(exNetwork), exNetwork, test=TRUE)
@@ -182,6 +111,21 @@ print.netcompare <- function(x, ...)
   print(x$edge)
 }
 
+# comparing and testing for (near) equality of networks
+#
+# Result of comparison:
+#
+# 1. computed network-level comparisons
+#
+# 2. built-in network-level comparisons
+#
+# 3. attributes
+#
+# 3a. attribute presence
+#
+# 3b. identical common attributes
+#
+# NOTE: Makes use of non-exported generic functions
 
 
 compareTest <- function(object)
@@ -196,3 +140,57 @@ compareTest <- function(object)
 }
 
 
+compareEdges <- function(target, current, use.names=FALSE)
+{
+  tr <- try(getS3method("as.matrix", class=class(target)), silent=TRUE)
+  if(inherits(tr, "try-error"))
+    stop("cannot find 'as.matrix' method for class ", dQuote(class(target)))
+  tr <- try(getS3method("as.matrix", class=class(current)), silent=TRUE)
+  if(inherits(tr, "try-error"))
+    stop("cannot find 'as.matrix' method for class ", dQuote(class(current)))
+  mtar <- as.matrix(target, "adjacency")
+  mcur <- as.matrix(current, "adjacency")
+  # compare matrices (no dimnames)
+  if(use.names)
+    all.equal(mtar, mcur)
+  else all.equal( structure(mtar, dimnames=NULL), structure(mcur,
+            dimnames=NULL) )
+}
+
+
+# compare common components of a list (by name)
+# return a list of all.equal results
+compareAlist <- function(target, current)
+{
+  # common components
+  nams <- intersect(names(target), names(current))
+  if( length(nams) == 0 )
+    return(as.character(NA))
+  rval <- lapply(nams, function(n) all.equal( target[[n]], current[[n]]) )
+  names(rval) <- nams
+  rval
+}
+
+
+
+# given a lists of attributes compare
+
+compareAttributes <- function(target, current)
+{
+  # compare number of attributes
+  rval <- list()
+  rval$n <- c(target=length(target), current=length(current))
+  # compare names
+  pre <- list()
+  u <- union(names(target), names(current))
+  r <- t(sapply(u, function(a)
+      c( a %in% names(target),
+          a %in% names(current) )
+      ))
+  pre <- c(pre, list(r))
+  pre <- do.call("rbind", pre)
+  dimnames(pre) <- list(rownames(pre), c("target", "current"))
+  rval$presence <- pre
+  rval$bycomp <- compareAlist(target, current)
+  structure(rval, class="netcomparea")
+}
