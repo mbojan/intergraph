e66ad6215c842e2a0142048b790aa543a50e5088
mbojan <mbojan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-20 02:22:28 +0000

    Updated tests for asNetwork
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/intergraph/pkg/intergraph@313 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/tests/test-asNetwork.R
+++ inst/tests/test-asNetwork.R
@@ -3,37 +3,37 @@ context("Creating networks from data frames")
 
 test_that("Disassembling directed network to d.f. and assembling back to network works", {
   l <- asDF(exNetwork)
-  g <- as.network( l$edges, vertices=l$vertexes)
+  g <- asNetwork( l$edges, vertices=l$vertexes)
   expect_that(g, is_identical_to(exNetwork))
 } )
 
 test_that("Disassembling undirected network to d.f. and assembling back to network works", {
   l <- asDF(exNetwork2)
-  g <- as.network( l$edges, vertices=l$vertexes, directed=FALSE)
+  g <- asNetwork( l$edges, vertices=l$vertexes, directed=FALSE)
   expect_that(g, is_identical_to(exNetwork2))
 } )
 
 test_that("Disassembling directed igraph to d.f. and assembling back to network: edgecount", {
   l <- asDF(exIgraph)
-  g <- as.network(l$edges, vertices=l$vertexes)
+  g <- asNetwork(l$edges, vertices=l$vertexes)
   expect_equal( network::network.edgecount(g), nrow(l$edges))
 } )
 
 test_that("Disassembling directed igraph to d.f. and assembling back to network: vcount", {
   l <- asDF(exIgraph)
-  g <- as.network(l$edges, vertices=l$vertexes)
+  g <- asNetwork(l$edges, vertices=l$vertexes)
   expect_equal( network::network.size(g), nrow(l$vertexes))
 } )
 
 test_that("Disassembling undirected igraph to d.f. and assembling back to network: edgecount", {
   l <- asDF(exIgraph2)
-  g <- as.network(l$edges, vertices=l$vertexes, directed=FALSE)
+  g <- asNetwork(l$edges, vertices=l$vertexes, directed=FALSE)
   expect_equal( network::network.edgecount(g), nrow(l$edges))
 } )
 
 test_that("Disassembling undirected igraph to d.f. and assembling back to network: vcount", {
   l <- asDF(exIgraph2)
-  g <- as.network(l$edges, vertices=l$vertexes, directed=FALSE)
+  g <- asNetwork(l$edges, vertices=l$vertexes, directed=FALSE)
   expect_equal( network::network.size(g), nrow(l$vertexes))
 } )
 
@@ -44,12 +44,12 @@ test_that("Disassembling undirected igraph to d.f. and assembling back to networ
 context("Converting igraphs to networks")
 
 test_that("Directed igraphs via netcompare", {
-  res <- netcompare( as.network(exIgraph), exIgraph, test=TRUE )
+  res <- netcompare( asNetwork(exIgraph), exIgraph, test=TRUE )
   expect_true(res)
 } )
 
 test_that("Undirected igraphs via netcompare", {
-  res2 <- netcompare( as.network(exIgraph2), exIgraph2, test=TRUE )
+  res2 <- netcompare( asNetwork(exIgraph2), exIgraph2, test=TRUE )
   expect_true(res2)
 } )
 
@@ -58,7 +58,7 @@ test_that("NAs in igraph vertex labels are copied (bug 1926)", {
   g <- igraph::graph( c(0,1, 1,2, 2,3, 3,4, 4,2)+1, directed=TRUE)
   igraph::E(g)$label <- c(1,2,3,NA,4)
   # convert to 'network'
-  net <- as.network(g)
+  net <- asNetwork(g)
   # warning that NA is converted to "NA"?
   expect_true( is.na(net %e% "label")[4] )
 } )
