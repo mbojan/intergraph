f02e1ddc6d7ca7ccf832707e7f6f0e8639eb489d
mbojan <mbojan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-20 02:22:26 +0000

    Updated examples of asNetwork
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/intergraph/pkg/intergraph@311 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- examples/asNetwork.R
+++ examples/asNetwork.R
@@ -1,13 +1,13 @@
-# require package 'network' as 'as.network' generic is defined there
+# require package 'network' as 'asNetwork' generic is defined there
 if(require(network, quietly=TRUE))
 {
   ### demonstrating method for data frames
   l <- asDF(exNetwork)
-  g <- as.network( l$edges, vertices=l$vertexes)
+  g <- asNetwork( l$edges, vertices=l$vertexes)
   stopifnot(all.equal(g, exNetwork))
 
   ### method for igraph objects
-  ig <- as.network(exIgraph)
+  ig <- asNetwork(exIgraph)
   identical( as.numeric(as.matrix(g, "adjacency")),
             as.numeric(as.matrix(ig, "adjacency")))
 }
