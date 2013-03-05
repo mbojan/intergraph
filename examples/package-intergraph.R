f8b63f0465dcdb73af56f89fb8885e2a7af4079c
mbojan <mbojan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-05 02:38:40 +0000

    Updated package examples
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/intergraph/pkg/intergraph@318 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- examples/package-intergraph.R
+++ examples/package-intergraph.R
@@ -1,31 +1,29 @@
-
 # example of converting objects between classes 'network' and 'igraph'
-
+# needs packages igraph and network attached
 if( require(network) & require(igraph) )
 {
 
   ### convert 'network' -> 'igraph'
 
-  # example network
+  # example network as object of class "network"
   summary(exNetwork)
 
-  # convert to igraph
+  # convert to class "igraph"
   g <- asIgraph(exNetwork)
 
   # check if 'exNetwork' and 'g' are the same
   all.equal( as.matrix(exNetwork, "edgelist"), 
-    igraph::get.edgelist(g) + 1 )   
-    # +1 because igraph's vertex ids start from 0
+    igraph::get.edgelist(g) )   
 
-  # compre results using 'netcompare'
+  # compare results using 'netcompare'
   netcompare(exNetwork, g)
 
   ### convert 'igraph' -> 'network'
 
-  # example network: same as above but of class 'igraph'
+  # example network as object of class "igraph"
   summary(exIgraph)
 
-  # convert to network class
+  # convert to class "network"
   gg <- asNetwork(exIgraph)
 
   # check if they are the same
