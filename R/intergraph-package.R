992cabb56e790ee622c8708eb167fe1483f05ce1
mbojan <mbojan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-05 02:38:43 +0000

    Updated package man page
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/intergraph/pkg/intergraph@320 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/intergraph-package.R
+++ R/intergraph-package.R
@@ -1,30 +1,37 @@
 #' Coercion routines for network data objects in R
 #' 
 #' This package contains methods for coercion between various classes used to
-#' represent network data in .
+#' represent network data in R.
 #' 
-#' Functions implemented in this package allow to coerce (i.e., convert) network
+#' Functions implemented in this package allow to coerce (i.e. convert) network
 #' data between classes provided by other R packages. Currently supported
-#' packages are: \pkg{network}, \pkg{igraph}.
+#' classes are: "network" from package \pkg{network}, "igraph" from package
+#' \pkg{igraph}.
 #' 
-#' The main functions are: \itemize{ \item \code{asNetwork} and its method
-#' \code{\link{asNetwork.igraph}} to create objects of class "network".
+#' The main functions are:
+#' \itemize{
+#' \item \code{\link{asNetwork}} and its methods to create objects of class
+#' "network".
 #' 
 #' \item \code{\link{asIgraph}} and its methods to create objects of class
-#' "igraph". } See their help pages for more information and examples.
+#' "igraph".
+#' }
+#' See their help pages for more information and examples.
 #' 
 #' As all the supported packages are written using S3 methods, so are the
-#' methods in this package. This may change in the future.
+#' methods in this package.
 #' 
 #' If you find this package useful in your work please cite it. Type
 #' \code{citation(package="intergraph")} for the information how to do that.
 #' 
-#' @name intergraph-package
-#' @aliases package-intergraph intergraph
 #' @importFrom network as.matrix.network
+#'
 #' @docType package
-#' @author Written and maintained by Michal Bojanowski
-#' \email{mbojan@@icm.edu.pl}.
+#' @name intergraph-package
+#'
+#' @author Written and maintained by Michal Bojanowski \email{m.bojanowski@@icm.edu.pl}.
+#'
 #' @keywords package
+#'
 #' @example examples/package-intergraph.R
 NULL
