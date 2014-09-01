ca9f0d4ddd5e8c1beaacb7d4608511cf8edcc9b9
Michal Bojanowski <michal2992@gmail.com>
2014-09-01 02:11:55 +0200

    Fixed attrmap test

--- inst/tests/test-attrmap.R
+++ inst/tests/test-attrmap.R
@@ -4,8 +4,9 @@ test_that("'na' vertex attribute can be dropped when making network", {
           net <- asNetwork(exIgraph)
           a <- attrmap()
           r <- data.frame(type="vertex", fromcls="network",
-                          fromattr="na", tocls="igraph", toattr=NA)
-          aa <- cbind(a, r)
+                          fromattr="na", tocls="igraph", toattr=NA,
+                          stringsAsFactors=FALSE)
+          aa <- rbind(a, r)
           g <- asIgraph(net, attrmap=aa)
           expect_false( "na" %in% igraph::list.vertex.attributes(g))
 } )
