0c5e0b5fc520e4233d0a4c0d033b7e557fccb028
Michal Bojanowski <michal2992@gmail.com>
2023-07-04 18:11:30 +0200

    Add sanity test of netcompare()
    References #34.

--- tests/testthat/test-netcompare.R
+++ tests/testthat/test-netcompare.R
@@ -1,18 +1,18 @@
-context("Testing compareAttributes")
+test_that("netcompare just works",{
+  expect_no_condition(
+    r <- netcompare(exIgraph, exIgraph)
+  )
+  expect_no_condition(print(r))
+})
 
 
 
 
-context("Test compareAList")
 
-# some lists
-l1 <- list(b=1:5, c=5:1)
-l2 <- list(a=2, b=1:5, c=1:5, d=3)
-
-          
+# compareAlist ------------------------------------------------------------
 
 test_that("compareAlist returns NA when intersection of names sets is empty", {
-          l1 <- list(a=2, b=1:5, c=1:5, d=3)
-          l2 <- list(B=1:5, C=5:1)
-          expect_identical(compareAlist(l1, l2), as.character(NA))
+  l1 <- list(a=2, b=1:5, c=1:5, d=3)
+  l2 <- list(B=1:5, C=5:1)
+  expect_identical(compareAlist(l1, l2), as.character(NA))
 } )
