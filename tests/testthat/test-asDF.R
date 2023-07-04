6f5f2a7f6c34fe38d02bb412b721bbb1a0856fa2
Michal Bojanowski <michal2992@gmail.com>
2023-07-04 17:53:40 +0200

    Refactor asDF() tests
    References #34.

--- tests/testthat/test-asDF.R
+++ tests/testthat/test-asDF.R
@@ -1,28 +1,46 @@
-context("Converting directed networks to data frames")
+# From 'network' objects --------------------------------------------------
 
-test_that("Created edgelist has appropriate number of edges", {
-	l <- asDF(exNetwork)
+test_that("asDF(exNetwork) returns data frames with proper no of rows", {
+  expect_silent(
+    l <- asDF(exNetwork)
+  )
+  # Created edgelist has appropriate number of edges
+  expect_equal(nrow(l$edges), network::network.edgecount(exNetwork))
+  # Created vertex data frame has correct number of vertices
+  expect_equal(nrow(l$vertexes), network::network.size(exNetwork))
+})
+
+test_that("asDF(exNetwork2) returns data frames with proper no of rows", {
+  expect_silent(
+    l <- asDF(exNetwork2)
+  )
+  # Created edgelist has appropriate number of edges
   expect_equal( nrow(l$edges), network::network.edgecount(exNetwork))
-} )
-
-test_that("Created vertex data frame has correct number of vertices", {
-	l <- asDF(exNetwork)
+  # Created vertex data frame has correct number of vertices
   expect_equal( nrow(l$vertexes), network::network.size(exNetwork))
-} )
+})
 
 
 
+# From 'igraph' objects ---------------------------------------------------
 
+test_that("asDF(exIgraph) returns data frames with proper no of rows", {
+  expect_silent(
+    l <- asDF(exIgraph)
+  )
+  # Created edgelist has appropriate number of edges
+  expect_equal(nrow(l$edges), igraph::ecount(exIgraph))
+  # Created vertex data frame has correct number of vertices
+  expect_equal(nrow(l$vertexes), igraph::vcount(exIgraph))
+})
 
+test_that("asDF(exIgraph2) returns data frames with proper no of rows", {
+  expect_silent(
+    l <- asDF(exIgraph2)
+  )
+  # Created edgelist has appropriate number of edges
+  expect_equal( nrow(l$edges), igraph::ecount(exIgraph2))
+  # Created vertex data frame has correct number of vertices
+  expect_equal( nrow(l$vertexes), igraph::vcount(exIgraph2))
+})
 
-context("Converting undirected networks to data frames")
-
-test_that("Created edgelist has appropriate number of edges", {
-	l <- asDF(exNetwork2)
-  expect_equal( nrow(l$edges), network::network.edgecount(exNetwork))
-} )
-
-test_that("Created vertex data frame has correct number of vertices", {
-	l <- asDF(exNetwork2)
-  expect_equal( nrow(l$vertexes), network::network.size(exNetwork))
-} )
