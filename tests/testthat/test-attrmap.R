2860030ef32926bf881dd7f9995b39091fba01a0
Michal Bojanowski <michal2992@gmail.com>
2023-07-05 01:26:08 +0200

    Refactor attrmap tests
    References #34.

--- tests/testthat/test-attrmap.R
+++ tests/testthat/test-attrmap.R
@@ -1,12 +1,17 @@
-context("Testing whether custom 'attrmap' specifications work")
+# Custom attrmap specifications -------------------------------------------
 
 test_that("'na' vertex attribute can be dropped when going network->igraph", {
-          net <- asNetwork(exIgraph)
-          a <- attrmap()
-          r <- data.frame(type="vertex", fromcls="network",
-                          fromattr="na", tocls="igraph", toattr=NA,
-                          stringsAsFactors=FALSE)
-          aa <- rbind(a, r)
-          g <- asIgraph(net, amap=aa)
-          expect_false( "na" %in% igraph::list.vertex.attributes(g))
+  net <- asNetwork(exIgraph)
+  a <- attrmap()
+  r <- data.frame(
+    type="vertex", 
+    fromcls="network",
+    fromattr="na",
+    tocls="igraph", 
+    toattr=NA,
+    stringsAsFactors=FALSE
+  )
+  aa <- rbind(a, r)
+  g <- asIgraph(net, amap=aa)
+  expect_false( "na" %in% igraph::list.vertex.attributes(g))
 } )
