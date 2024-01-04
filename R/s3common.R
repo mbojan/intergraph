46572bfd4a97b461f9352a639e414a313903c9ad
Michał Bojanowski <michal2992@gmail.com>
2024-01-04 17:11:26 +0100

    Revert "chore: Use `@export` for internal methods"
    This reverts commit d295058d9415b45b56379d01360d919c9eb71a8f.

--- R/s3common.R
+++ R/s3common.R
@@ -4,33 +4,29 @@
 
 igVcount <- function(x, ...) UseMethod("igVcount")
 
-#' @export
 igVcount.igraph <- function(x, ...)
   igraph::vcount(x)
 
-#' @export
 igVcount.network <- function(x, ...)
   network::network.size(x)
 
 igEcount <- function(x, ...) UseMethod("igEcount")
 
-#' @export
 igEcount.igraph <- function(x, ...)
   igraph::ecount(x)
 
-#' @export
 igEcount.network <- function(x, ...)
   network::network.edgecount(x)
 
 igDirected <- function(x) UseMethod("igDirected")
 
-#' @export
-igDirected.igraph <- function(x) {
-  igraph::is_directed(x)
+igDirected.igraph <- function(x)
+{
+  igraph::is.directed(x)
 }
 
-#' @export
-igDirected.network <- function(x) {
+igDirected.network <- function(x)
+{
   network::is.directed(x)
 }
 
